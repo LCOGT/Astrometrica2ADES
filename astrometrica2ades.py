677b3c51516dd33322ec14547a6c3d18de11305c
Tim Lister <tlister@lcogt.net>
2018-02-16 21:27:33 -0700

    Add support for OBS line

--- astrometrica2ades.py
+++ astrometrica2ades.py
@@ -9,14 +9,20 @@ except ImportError:
 def parse_header(header_lines):
 
     version_string = "# version=2017"
-    observatory = ''
+    observatory = observers = ''
 
     if type(header_lines) != list:
         header_lines = [header_lines,]
     for line in header_lines:
         if line[0:3] == 'COD':
             observatory = parse_obscode(line[4:])
-    header = version_string + '\n' + observatory
+        elif line[0:3] == 'OBS':
+            observers = parse_observers(line[4:])
+    header = version_string + '\n'
+    if observatory != '':
+        header += observatory
+    if observers != '':
+        header += observers
     return header
 
 def parse_obscode(code_line):
@@ -34,3 +40,14 @@ def parse_obscode(code_line):
         observatory += "! name " + site_name +"\n"
 
     return observatory
+
+def parse_observers(code_line):
+
+    observers = ''
+
+    obs = code_line.split(',')
+    if len(obs) >= 1:
+        observers = '# observers\n'
+        for observer in obs:
+            observers += "! name " + observer.strip() + "\n"
+    return observers
--- tests/test_psv.py
+++ tests/test_psv.py
@@ -28,3 +28,33 @@ class Test_ParseHeader():
         header = parse_header(header_line)
 
         assert expected_header == header
+
+    def test_OBS_header(self):
+
+        expected_header = ("# version=2017" + "\n"
+                           "# observers" + "\n"
+                           "! name R. L. Seaman" + "\n"
+                           )
+
+        header_line = "OBS R. L. Seaman"
+
+        header = parse_header(header_line)
+
+        assert expected_header == header
+
+    def test_OBS_header_multi_observers(self):
+
+        expected_header = ("# version=2017" + "\n"
+                           "# observers" + "\n"
+                           "! name R. L. Seaman" + "\n"
+                           "! name E. J. Christensen" + "\n"
+                           "! name D. C. Fuls" + "\n"
+                           "! name A. R. Gibbs" + "\n"
+
+                           )
+
+        header_line = "OBS R. L. Seaman, E. J. Christensen, D. C. Fuls, A. R. Gibbs"
+
+        header = parse_header(header_line)
+
+        assert expected_header == header
