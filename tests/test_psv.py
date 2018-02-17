0689dc6f11b92205c27eb4b672cf27e03d22d213
Tim Lister <tlister@lcogt.net>
2018-02-17 12:52:23 -0700

    Add full header parsing test

--- tests/test_psv.py
+++ tests/test_psv.py
@@ -1,6 +1,20 @@
+import os
+
 from astrometrica2ades import parse_header
 
-class Test_ParseHeader():
+class Test_ParseHeader(object):
+
+    def setup_method(self):
+        test_mpcreport = os.path.join('tests', 'data', 'MPCReport.txt')
+        test_fh = open(test_mpcreport, 'r')
+        self.header = []
+        self.body = []
+        for line in test_fh:
+            if line[0:3] in ['COD','CON','OBS','MEA','TEL','ACK','AC2','COM','NET']:
+                self.header.append(line)
+            else:
+                self.body.append(line)
+        test_fh.close()
 
     def test_COD_header(self):
 
@@ -115,5 +129,24 @@ class Test_ParseHeader():
         header_line = "TEL 1.5-m f/6 reflector + CCD"
 
         header = parse_header(header_line)
+
+        assert expected_header == header
+
+    def test_full_header(self):
+        expected_header = ( "# version=2017" + "\n"
+                            "# observatory" + "\n"
+                            "! mpcCode W85" + "\n"
+                            "# observers" + "\n"
+                            "! name T. Lister" + "\n"
+                            "# measurers" + "\n"
+                            "! name T. Lister" + "\n"
+                            "# telescope" + "\n"
+                            "! aperture 1.0" + "\n"
+                            "! design Ritchey-Chretien" + "\n"
+                            "! detector CCD" + "\n"
+                            "! fRatio f/8" + "\n"
+                           )
+
+        header = parse_header(self.header)
 
         assert expected_header == header
